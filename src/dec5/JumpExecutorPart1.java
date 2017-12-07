3a2421d052850eb60f38b5154e6cb372340f1ba3
Yasmeen Akbari <yasmeena@appnovation.com>
2017-12-06 19:54:15 -0800

    finish dec5 parts 1 and 2

---
+++ src/dec5/JumpExecutorPart1.java
@@ -0,0 +1,50 @@
+package dec5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JumpExecutorPart1 {
+
+	private static int calculateSteps(List<Integer> stepList) {
+		int numSteps, curPos, numJumps;
+		curPos = 0;
+		numSteps = 0;
+		
+		while ( (curPos >= 0) &&  (curPos <= stepList.size()-1)) {
+			numJumps = stepList.get(curPos);
+			stepList.set(curPos, numJumps+1);
+			curPos = curPos + numJumps;
+			numSteps = numSteps + 1;
+		}
+		
+		return numSteps;
+	}
+
+	public static void main(String[] args) {
+		File input = new File("src/Dec5/input.txt");
+		List<Integer> stepList = new ArrayList<Integer>();
+		int numSteps;
+		
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(input));
+			String stepString;
+			
+			while ((stepString = reader.readLine()) != null){
+				stepList.add(Integer.parseInt(stepString));
+			}
+			
+			numSteps = calculateSteps(stepList);
+			System.out.println(numSteps);
+			reader.close();
+					
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
